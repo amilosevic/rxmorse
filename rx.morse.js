286340c65bc0cbf712f91610dae97f1f0420d0f8
Aleksandar Milosevic <milosevic.aca@gmail.com>
2016-02-17 07:41:54 +0100

    formatiranje koda

--- rx.morse.js
+++ rx.morse.js
@@ -80,6 +80,27 @@ RxMorse = (function () {
                         }
                     );
 
+                    xmod20 = {
+                        value: cr,
+                        timestamp: x.timestamp
+                    };
+
+                    Rx.Observable.just(xmod20)
+                        .delay(20 * unit * 0.9)
+                        .subscribe(
+                        function (x1) {
+                            if (x1.timestamp == last) {
+                                //last = null;
+                                x1.__timestamp = Date.now();
+                                //console.log(x1);
+                                subject.onNext(x1);
+                                if (completed) {
+                                    subject.onCompleted();
+                                }
+                            }
+                        }
+                    );
+
 
                 }
 
@@ -104,45 +125,45 @@ RxMorse = (function () {
         var end = '';
         var huffmanIn = {
             // 0 level
-            '*' : { '===' : 'T', '=' : 'E'}, // _top_
+            '*': {'===': 'T', '=': 'E'}, // _top_
 
             // I level
-            'T' : { '===' : 'M', '=' : 'N'},
-            'E' : { '===' : 'A', '=' : 'I'},
+            'T': {'===': 'M', '=': 'N'},
+            'E': {'===': 'A', '=': 'I'},
 
             // II level
-            'M' : { '===' : 'O', '=' : 'G'},
-            'N' : { '===' : 'K', '=' : 'D'},
-            'A' : { '===' : 'W', '=' : 'R'},
-            'I' : { '===' : 'U', '=' : 'S'},
+            'M': {'===': 'O', '=': 'G'},
+            'N': {'===': 'K', '=': 'D'},
+            'A': {'===': 'W', '=': 'R'},
+            'I': {'===': 'U', '=': 'S'},
 
             // III level
-            'O' : { '===' :'CH', '=' : 'Ö'},
-            'G' : { '===' : 'Q', '=' : 'Z'},
-            'K' : { '===' : 'Y', '=' : 'C'},
-            'D' : { '===' : 'X', '=' : 'B'},
-            'W' : { '===' : 'J', '=' : 'P'},
-            'R' : { '===' : 'Ä', '=' : 'L'},
-            'U' : { '===' : 'Ü', '=' : 'F'},
-            'S' : { '===' : 'V', '=' : 'H'},
+            'O': {'===': 'CH', '=': 'Ö'},
+            'G': {'===': 'Q', '=': 'Z'},
+            'K': {'===': 'Y', '=': 'C'},
+            'D': {'===': 'X', '=': 'B'},
+            'W': {'===': 'J', '=': 'P'},
+            'R': {'===': 'Ä', '=': 'L'},
+            'U': {'===': 'Ü', '=': 'F'},
+            'S': {'===': 'V', '=': 'H'},
 
             // IV level
-            'CH': { '===' : '0', '=' : '9'},
-            'Ö' : { '===' : end, '=' : '8'},
-            'Q' : { '===' : 'Ñ', '=' : 'Ĝ'},
-            'Z' : { '===' :'_Z', '=' : '7'},
-            'Y' : { '===' : end, '=' : 'Ĥ'},
-            'C' : { '===' :'_C', '=' : 'Ç'},
-            'X' : { '===' : end, '=' : '/'},
-            'B' : { '===' : '=', '=' : '6'},
-            'J' : { '===' : '1', '=' : 'Ĵ'},
-            'P' : { '===' : 'Á', '=' : 'Þ'},
-            'Ä' : { '===' : end, '=' : '+'},
-            'L' : { '===' : 'È', '=' : end},
-            'Ü' : { '===' : '2', '=' : 'Đ'},
-            'F' : { '===' : end, '=' : 'É'},
-            'V' : { '===' : '3', '=' : 'Ŝ'},
-            'H' : { '===' : '4', '=' : '5'}//,
+            'CH': {'===': '0', '=': '9'},
+            'Ö': {'===': end, '=': '8'},
+            'Q': {'===': 'Ñ', '=': 'Ĝ'},
+            'Z': {'===': '_Z', '=': '7'},
+            'Y': {'===': end, '=': 'Ĥ'},
+            'C': {'===': '_C', '=': 'Ç'},
+            'X': {'===': end, '=': '/'},
+            'B': {'===': '=', '=': '6'},
+            'J': {'===': '1', '=': 'Ĵ'},
+            'P': {'===': 'Á', '=': 'Þ'},
+            'Ä': {'===': end, '=': '+'},
+            'L': {'===': 'È', '=': end},
+            'Ü': {'===': '2', '=': 'Đ'},
+            'F': {'===': end, '=': 'É'},
+            'V': {'===': '3', '=': 'Ŝ'},
+            'H': {'===': '4', '=': '5'}//,
 
             // V level
             // @todo .. clean up previous levels to confirm to ITU M.1677 : International Morse code
@@ -153,45 +174,45 @@ RxMorse = (function () {
 
         var huffmanOut = {
 
-            'A' : [dit, dah],
-            'B' : [dah, dit, dit, dit],
-            'C' : [dah, dit, dah, dit],
-            'D' : [dah, dit, dit],
-            'E' : [dit],
-            'F' : [dit, dit, dah, dit],
-            'G' : [dah, dah, dit],
-            'H' : [dit, dit, dit, dit],
-            'I' : [dit, dit],
-            'J' : [dit, dah, dah, dah],
-            'K' : [dah, dit, dah],
-            'L' : [dit, dah, dit, dit],
-            'M' : [dah, dah],
-            'N' : [dah, dit],
-            'O' : [dah, dah, dah],
-            'P' : [dit, dah, dah, dit],
-            'Q' : [dah, dah, dit, dah],
-            'R' : [dit, dah, dit],
-            'S' : [dit, dit, dit],
-            'T' : [dah],
-            'U' : [dit, dit, dah],
-            'V' : [dit, dit, dit, dah],
-            'W' : [dit, dah, dah],
-            'X' : [dah, dit, dit, dah],
-            'Y' : [dah, dit, dah, dah],
-            'Z' : [dah, dah, dit, dit],
+            'A': [dit, dah],
+            'B': [dah, dit, dit, dit],
+            'C': [dah, dit, dah, dit],
+            'D': [dah, dit, dit],
+            'E': [dit],
+            'F': [dit, dit, dah, dit],
+            'G': [dah, dah, dit],
+            'H': [dit, dit, dit, dit],
+            'I': [dit, dit],
+            'J': [dit, dah, dah, dah],
+            'K': [dah, dit, dah],
+            'L': [dit, dah, dit, dit],
+            'M': [dah, dah],
+            'N': [dah, dit],
+            'O': [dah, dah, dah],
+            'P': [dit, dah, dah, dit],
+            'Q': [dah, dah, dit, dah],
+            'R': [dit, dah, dit],
+            'S': [dit, dit, dit],
+            'T': [dah],
+            'U': [dit, dit, dah],
+            'V': [dit, dit, dit, dah],
+            'W': [dit, dah, dah],
+            'X': [dah, dit, dit, dah],
+            'Y': [dah, dit, dah, dah],
+            'Z': [dah, dah, dit, dit],
 
-            '1' : [dit, dah, dah, dah, dah],
-            '2' : [dit, dit, dah, dah, dah],
-            '3' : [dit, dit, dit, dah, dah],
-            '4' : [dit, dit, dit, dit, dah],
-            '5' : [dit, dit, dit, dit, dit],
-            '6' : [dah, dit, dit, dit, dit],
-            '7' : [dah, dah, dit, dit, dit],
-            '8' : [dah, dah, dah, dit, dit],
-            '9' : [dah, dah, dah, dah, dit],
-            '0' : [dah, dah, dah, dah, dah],
+            '1': [dit, dah, dah, dah, dah],
+            '2': [dit, dit, dah, dah, dah],
+            '3': [dit, dit, dit, dah, dah],
+            '4': [dit, dit, dit, dit, dah],
+            '5': [dit, dit, dit, dit, dit],
+            '6': [dah, dit, dit, dit, dit],
+            '7': [dah, dah, dit, dit, dit],
+            '8': [dah, dah, dah, dit, dit],
+            '9': [dah, dah, dah, dah, dit],
+            '0': [dah, dah, dah, dah, dah],
 
-            ' ' : [sssssss]
+            ' ': [sssssss]
         };
 
         var unit = 130;
@@ -200,23 +221,30 @@ RxMorse = (function () {
         var ticker = document.getElementById(tickersel);
 
         // event sources
-        var mousedown = Rx.Observable.fromEvent(pad, 'mousedown').filter(function (e) {return e.button == 0});
-        var mouseup = Rx.Observable.fromEvent(pad, 'mouseup').filter(function (e) {return e.button == 0});
-        var keydown = Rx.Observable.fromEvent(document, 'keydown').filter(function (e) {return e.which == 32; });
-        var keyup = Rx.Observable.fromEvent(document, 'keyup').filter(function (e) {return e.which == 32;});
-
+        var mousedown = Rx.Observable.fromEvent(pad, 'mousedown').filter(function (e) { return e.button == 0 });
+        var mouseup = Rx.Observable.fromEvent(pad, 'mouseup').filter(function (e) { return e.button == 0 });
+        var keydown = Rx.Observable.fromEvent(document, 'keydown').filter(function (e) { return e.which == 32; });
+        var keyup = Rx.Observable.fromEvent(document, 'keyup').filter(function (e) { return e.which == 32; });
 
 
         var mouse = Rx.Observable.merge(mousedown, mouseup)
-            .do(function(e) {e.preventDefault()})
-            .map(function(e) {return e.type})
+            .do(function (e) {
+                e.preventDefault()
+            })
+            .map(function (e) {
+                return e.type
+            })
             .distinctUntilChanged()
             .timestamp();
 
 
         var keyboard = Rx.Observable.merge(keydown, keyup)
-            .do(function(e) {e.preventDefault()})
-            .map(function(e) {return e.type})
+            .do(function (e) {
+                e.preventDefault()
+            })
+            .map(function (e) {
+                return e.type
+            })
             .distinctUntilChanged()
             .timestamp();
 
@@ -226,7 +254,7 @@ RxMorse = (function () {
                 if (x in huffmanOut) {
                     return Rx.Observable.from(huffmanOut[x].concat(sss));
                 } else {
-                    return Rx.Observable.throw(new Error('** ' + x ));
+                    return Rx.Observable.throw(new Error('** ' + x));
                 }
             })
             .concatMap(function (x) {
@@ -234,64 +262,53 @@ RxMorse = (function () {
                     case dit:
                         return Rx.Observable.concat(
                             Rx.Observable.just("robotdown"),
-                            Rx.Observable.just("robotup").delay(1*unit),
-                            Rx.Observable.empty().delay(1*unit)
+                            Rx.Observable.just("robotup").delay(1 * unit),
+                            Rx.Observable.empty().delay(1 * unit)
                         );
                     case dah:
                         return Rx.Observable.concat(
                             Rx.Observable.just("robotdown"),
-                            Rx.Observable.just("robotup").delay(3*unit),
-                            Rx.Observable.empty().delay(1*unit)
+                            Rx.Observable.just("robotup").delay(3 * unit),
+                            Rx.Observable.empty().delay(1 * unit)
                         );
                     case sss:
-                        return Rx.Observable.empty().delay(3*unit);
+                        return Rx.Observable.empty().delay(3 * unit);
                     case sssssss:
-                        return Rx.Observable.empty().delay(7*unit);
+                        return Rx.Observable.empty().delay(7 * unit);
                     default:
                         return Rx.Observable.throw(new Error('***'));
                 }
             })
             .timestamp();
 
-        //robot.subscribe(
-        //    function (x) { console.log(x); },
-        //    function (e) { console.log("Error: " + e)},
-        //    function () { console.log('Complete'); }
-        //);
+
 
         var inputs = Rx.Observable.merge(mouse, keyboard, robot);
 
         var spacer = subjectize(inputs, unit);
 
-        var merged = Rx.Observable.merge(inputs, spacer)
-        var source = merged
+        var source = Rx.Observable.merge(inputs, spacer)
             .map(function (a) {
-
-                var typ;
-
                 switch (a.value) {
                     case 'robotdown':
                     case 'keydown':
                     case 'mousedown':
-                        typ = 'down'; break;
+                        return 'down';
                     case 'keyup':
                     case 'robotup':
                     case 'mouseup':
-                        typ = 'up'; break;
+                        return 'up';
                     case ls:
                     case ws:
-                        typ = a.value; break;
-
+                    case cr:
+                        return a.value;
                     default:
-                        typ = 'forgetaboutit'
+                        throw new Error('!');
                 }
-
-                return typ;
-            }).distinctUntilChanged();
+            });
 
         var symbols = source.timeInterval()
             .map(function (e) {
-                //console.log("-->" + e.interval + " // " + e.value);
                 if (e.value == 'up') {
                     if (e.interval < 1.5 * unit) {
                         return dit;
@@ -299,82 +316,61 @@ RxMorse = (function () {
                         return dah;
                     }
                 } else if (e.value == 'down') {
-                    return '.';
+                    return null;
                 } else if (e.value == ls) {
                     return ls;
                 } else if (e.value == ws) {
                     return ws;
+                } else if (e.value == cr) {
+                    return cr;
                 } else {
                     console.log('unhandled: ' + e.value);
                 }
-            }).filter(function (x) {return x != '.'});
+            }).filter(function (x) {
+                return x != null;
+            });
 
 
-        var transformed = Rx.Observable.concat(
-            symbols
-        );
+        var out = symbols.scan(
+            function (acc, x, i, source) {
 
-        var out = transformed.scan(
-                function (acc, x, i, source) {
+                switch (x) {
+                    default:
+                    case ss:
+                        throw new Error("Could not handle: " + x);
+                    case ls:
+                        return {action: 'out', state: acc.state}; // reset state machine
+                    case ws:
+                        return {action: 'out', state: ' '}; // space
+                    case cr:
+                        return {action: 'out', state: "\n"}; // carrige return
 
-                    switch (x){
-                        default:
-                        case ss:
-                            throw new Error("Could not handle: " + x);
-                        case ls:
-                            return {action : 'out', state : acc.state}; // reset state machine
-                        case ws:
-                            return {action : 'out', state : ' '}; // space
-                        case cr:
-                            return {action : 'out', state : "\n"}; // carrige return
+                    case dit:
+                    case dah:
+                        var key;
+                        if (acc.action == 'out') {
+                            key = _top_;
+                        } else {
+                            key = acc.state;
+                        }
 
-                        case dit:
-                        case dah:
-                            var key;
-                            if (acc.action == 'out') {
-                                key = _top_;
-                            } else {
-                                key = acc.state;
-                            }
+                        var a = huffmanIn[key];
+                        if (a == undefined) {
+                            return {action: 'out', state: _top_}
+                        }
 
-                            var a = huffmanIn[key];
-                            if (a == undefined) {
-                                return {action : 'out', state: _top_ }
-                            }
+                        return {action: 'wait', state: a[x]};
 
-                            return {action: 'wait', state: a[x]};
-
-                    }
-                },
+                }
+            },
             {action: 'wait', state: _top_}
-        ).filter(function (x) {return x.action == 'out'})
-            .map(function (x) {return x.state});
+        ).filter(function (x) {
+                return x.action == 'out'
+            })
+            .map(function (x) {
+                return x.state
+            });
 
-
-        //var subscription1 = Rx.Observable.zipArray(transformed, out).subscribe(
-        //    function (x) {
-        //        console.log(x)
-        //    }
-        //);
-
-        //var subscription2 = keyboardSubject.subscribe(function (e) {
-        //   console.log(e.timestamp + " // " + e.value);
-        //});
-
-        //var subscription2 = source.timestamp().subscribe(function (e) {
-        //    console.log('source: ' + e.timestamp + " // " + e.value);
-        //});
-
-        //var subscription3 = symbols.timestamp().subscribe(
-        //    function (x) { console.log('symbols: ' + x.timestamp + " // " + x.value); },
-        //    function (e) { console.log('error: ' + e)},
-        //    function () { console.log('onCompleted')}
-        //);
-        var subscription3 = out.subscribe(
-            function (x) { console.log('out:' + x); },
-            function (e) { console.log('error: ' + e)},
-            function () { console.log('onCompleted')}
-        );
 
         var subscription4 = source.subscribe(
             function (x) {
@@ -387,7 +383,11 @@ RxMorse = (function () {
         );
 
 
-        //
+        var subscription5 = out.subscribe(
+          function (x) {
+              ticker.innerHTML += (x == "\n" ? '<br/>' : x);
+          }
+        );
 
 
     };
